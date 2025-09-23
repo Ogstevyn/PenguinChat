03e7a1b5831b203297e868cac58643d73cb087a6
0x-Cozy <xcozydegen@gmail.com>
2025-09-23 18:07:33 +0100

    settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(), 
-    wasm(),
+    react(),
     mode === "development" && componentTagger()
   ].filter(Boolean),
   resolve: {
