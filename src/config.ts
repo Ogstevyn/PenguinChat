31f2aa6e7e5d7a165ad310ab0055f162b8685557
0x-Cozy <xcozydegen@gmail.com>
2025-09-25 03:03:24 +0100

    conclusion/gifting

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const LITE_SERVER = import.meta.env.VITE_ENV === 'production' 
-  ? 'https://penguinchat-backend.onrender.com'
+  ? 'https://penguinchat.onrender.com'
   : 'http://localhost:3002';
